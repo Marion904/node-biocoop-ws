aeb7fbf8bb07cb3178b2cfbb88f5d93fccb7026e
marion <marionfontaine904@gmail.com>
2017-10-30 23:30:02 +0100

    adding cors module 4

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ var app = express();
 
 
 
-app.listen(1024, function () {
-  console.log('CORS-enabled web server listening on port 1024')
-})
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(logger);
 
-app.use(cors());
 
+app.use(cors());
 
 app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(1024, function () {
+  console.log('CORS-enabled web server listening on port 1024')
 })
 
 app.use(express.static(__dirname + '/public'));
